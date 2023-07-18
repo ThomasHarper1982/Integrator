b6729811afb557a39a0b90dc337396c0af769e19
ThomasHarper1982 <thomasjharper@hotmail.com>
2023-07-18 23:19:32 +0100

    Removed ignored files

--- _deps/rapidxml-build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/thomas/Documents/Projects/Integrator/_deps/rapidxml-src
-# Build directory: /home/thomas/Documents/Projects/Integrator/_deps/rapidxml-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/CPM_modules/Findrapidxml.cmake
+++
@@ -1,3 +0,0 @@
-include("/home/thomas/Documents/Projects/Integrator/build/cmake/CPM_0.38.1.cmake")
-CPMAddPackage("GITHUB_REPOSITORY;CodeFinder2/rapidxml;VERSION;1.13;EXCLUDE_FROM_ALL;YES;SYSTEM;YES;")
-set(rapidxml_FOUND TRUE)
